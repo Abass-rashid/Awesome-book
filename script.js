57524d87cd569193c0a342640f5aaeb15efb577c
abass rashid <abazrashid144@gmail.com>
2022-07-04 17:48:43 +0300

    Fix linters errors

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-if (localStorage.getItem("data") === null) {
-  localStorage.setItem("data", "[]");
+if (localStorage.getItem('data') === null) {
+  localStorage.setItem('data', '[]');
 }
 
 let bookList = [];
@@ -10,25 +10,25 @@ function Book(id, title, author) {
   this.author = author;
 }
 function addBooks(bookList) {
-  const books = document.getElementById("books");
-  const liToRemove = document.querySelectorAll("li");
+  const books = document.getElementById('books');
+  const liToRemove = document.querySelectorAll('li');
   liToRemove.forEach((item) => {
     item.remove();
   });
 
   bookList.forEach((book) => {
-    const li = document.createElement("li");
-    const hr = document.createElement("hr");
-    const removeBtn = document.createElement("input");
+    const li = document.createElement('li');
+    const hr = document.createElement('hr');
+    const removeBtn = document.createElement('input');
     removeBtn.id = book.id;
-    removeBtn.className = "remove-btn";
-    removeBtn.setAttribute("onclick", `removeBook( ${book.id} )`);
-    removeBtn.setAttribute("type", "button");
-    removeBtn.setAttribute("value", "Remove");
-    const titleBlock = document.createElement("div");
+    removeBtn.className = 'remove-btn';
+    removeBtn.setAttribute('onclick', `removeBook( ${book.id} )`);
+    removeBtn.setAttribute('type', 'button');
+    removeBtn.setAttribute('value', 'Remove');
+    const titleBlock = document.createElement('div');
     const title = document.createTextNode(book.title);
     titleBlock.appendChild(title);
-    const authorBlock = document.createElement("div");
+    const authorBlock = document.createElement('div');
     const author = document.createTextNode(book.author);
     authorBlock.appendChild(author);
 
@@ -41,20 +41,20 @@ function addBooks(bookList) {
 }
 
 function getFromLocalStorage() {
-  return JSON.parse(localStorage.getItem("data"));
+  return JSON.parse(localStorage.getItem('data'));
 }
 
 function saveToLocalStorage(bookList) {
-  localStorage.setItem("data", JSON.stringify(bookList));
+  localStorage.setItem('data', JSON.stringify(bookList));
   addBooks(getFromLocalStorage());
 }
 
-const addBtn = document.getElementById("add");
-addBtn.addEventListener("click", () => {
+const addBtn = document.getElementById('add');
+addBtn.addEventListener('click', () => {
   bookList = getFromLocalStorage();
   const id = Date.now();
-  const title = document.getElementById("title").value;
-  const author = document.getElementById("author").value;
+  const title = document.getElementById('title').value;
+  const author = document.getElementById('author').value;
   const book = new Book(id, title, author);
   bookList.push(book);
   saveToLocalStorage(bookList);
@@ -68,7 +68,5 @@ function removeBook(id) {
 
 addBooks(getFromLocalStorage());
 
-const removeBtns = document.querySelectorAll("remove-btn");
-removeBtns.forEach((btn) =>
-  btn.addEventListener("click", (ev) => removeBook(ev.target.id))
-);
+const removeBtns = document.querySelectorAll('remove-btn');
+removeBtns.forEach((btn) => btn.addEventListener('click', (ev) => removeBook(ev.target.id)));
