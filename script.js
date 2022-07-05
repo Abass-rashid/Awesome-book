186b32635ff58a01177c5f137628213aed68cf82
abass rashid <abazrashid144@gmail.com>
2022-07-05 14:48:15 +0300

    Fix linters error

--- script.js
+++ script.js
@@ -15,28 +15,28 @@ class BookList {
 const bookList = new BookList();
 
 function updateView() {
-  const books = document.getElementById("books");
-  const liToRemove = document.querySelectorAll("li");
+  const books = document.getElementById('books');
+  const liToRemove = document.querySelectorAll('li');
   liToRemove.forEach((item) => {
     item.remove();
   });
   bookList.books.forEach((book) => {
-    const li = document.createElement("li");
-    const hr = document.createElement("hr");
-    const removeBtn = document.createElement("input");
+    const li = document.createElement('li');
+    const hr = document.createElement('hr');
+    const removeBtn = document.createElement('input');
     removeBtn.id = book.id;
-    removeBtn.className = "remove-btn";
-    removeBtn.addEventListener("click", (ev) => {
+    removeBtn.className = 'remove-btn';
+    removeBtn.addEventListener('click', (ev) => {
       bookList.remove(ev.target.id);
-      localStorage.setItem("data", JSON.stringify(bookList.books));
+      localStorage.setItem('data', JSON.stringify(bookList.books));
       this.updateView();
     });
-    removeBtn.setAttribute("type", "button");
-    removeBtn.setAttribute("value", "Remove");
-    const titleBlock = document.createElement("div");
+    removeBtn.setAttribute('type', 'button');
+    removeBtn.setAttribute('value', 'Remove');
+    const titleBlock = document.createElement('div');
     const title = document.createTextNode(book.title);
     titleBlock.appendChild(title);
-    const authorBlock = document.createElement("div");
+    const authorBlock = document.createElement('div');
     const author = document.createTextNode(book.author);
     authorBlock.appendChild(author);
     li.appendChild(titleBlock);
@@ -47,21 +47,21 @@ function updateView() {
   });
 }
 
-const addBtn = document.getElementById("add");
-addBtn.addEventListener("click", () => {
-  const title = document.getElementById("title").value;
-  const author = document.getElementById("author").value;
+const addBtn = document.getElementById('add');
+addBtn.addEventListener('click', () => {
+  const title = document.getElementById('title').value;
+  const author = document.getElementById('author').value;
   bookList.add(title, author);
-  localStorage.setItem("data", JSON.stringify(bookList.books));
+  localStorage.setItem('data', JSON.stringify(bookList.books));
   updateView();
-  document.getElementById("title").value = "";
-  document.getElementById("author").value = "";
+  document.getElementById('title').value = '';
+  document.getElementById('author').value = '';
 });
 
-if (localStorage.getItem("data") === null) {
-  localStorage.setItem("data", "[]");
+if (localStorage.getItem('data') === null) {
+  localStorage.setItem('data', '[]');
 }
 
-bookList.books = JSON.parse(localStorage.getItem("data"));
+bookList.books = JSON.parse(localStorage.getItem('data'));
 
 updateView();
